4ae0dd7cf1370562335ec23d886283a3bcdb52d3
Switch-Thomas <admiralswitch@gmail.com>
2014-12-07 10:26:46 -0800

    changed db to postgress for heroku and adjusted the migrate file

--- db/migrate/20141207175154_create_users.rb
+++ db/migrate/20141207175154_create_users.rb
@@ -9,6 +9,7 @@ class CreateUsers < ActiveRecord::Migration
 
       t.timestamps
     end
+
   add_index :users, :uid
   add_index :users, :provider
   add_index :users, [:uid, :provider], unique: true
